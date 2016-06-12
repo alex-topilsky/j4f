b1dd1832c038f6e8534fe9353e7c65db58a2555d
Alexander_Topilskii <alex.topilsky@gmail.com>
2016-06-12 03:20:00 +0300

    Revert "first step in java 8"
    This reverts commit c2ff58578c16aad3a090eaf89a44c7b32420b300.

--- src/Generator.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.Stream;
-
-public class Generator {
-	
-	public static void show() {
-		Stream.generate(() -> Math.random()).forEach(System.out::println);
-	}
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-import java.util.Random;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//Generator.show();
-		Runnable r = () -> System.out.println("hello world");
-		Runnable r2 = () -> Stream.generate(() -> (Math.random() + 5)).forEach(System.out::println);
-		Runnable r1 = () -> Stream.generate(() -> Math.random()).forEach(System.out::println);
-		new Thread(r1).start();
-		new Thread(r2).start();	
-		
+		// TODO Auto-generated method stub
+			System.out.println("Hello");
 	}
 
 }
